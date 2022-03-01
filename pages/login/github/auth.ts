3b79b6be19173669e282cc02b482b12300285857
y-fujiwara <fuji.ys5234@gmail.com>
2022-03-02 01:15:01 +0900

    add log

--- pages/login/github/auth.ts
+++ pages/login/github/auth.ts
@@ -2,16 +2,21 @@ import { buildGithubUrl, tokenEncrypt } from "../../../logics/github.ts";
 import { HandlerContext } from "../../../server_deps.ts";
 
 export async function handler(_ctx: HandlerContext): Promise<Response> {
-  const oauthUrl = buildGithubUrl();
-  const githubRedirect = new Response(null, {
-    status: 302,
-    headers: {
-      location: oauthUrl,
-      "set-cookie": `state=${await tokenEncrypt(
-        new URL(oauthUrl).searchParams.get("state") || ""
-      )};HttpOnly;SameSite=Lax;Path=/;`,
-    },
-  });
+  try {
+    const oauthUrl = buildGithubUrl();
+    const githubRedirect = new Response(null, {
+      status: 302,
+      headers: {
+        location: oauthUrl,
+        "set-cookie": `state=${await tokenEncrypt(
+          new URL(oauthUrl).searchParams.get("state") || "",
+        )};HttpOnly;SameSite=Lax;Path=/;`,
+      },
+    });
 
-  return githubRedirect;
+    return githubRedirect;
+  } catch (e) {
+    console.error(e);
+    return new Response("Internal Server Error", { status: 500 });
+  }
 }
